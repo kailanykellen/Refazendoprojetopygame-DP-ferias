9c77615c2dce820da52a108610e3b5189081aa1c
Kailany <kailanykmm@al.insper.edu.br>
2021-08-12 07:03:34 -0300

    exercicio 3 handout

--- jogo.py
+++ jogo.py
@@ -8,11 +8,13 @@ pygame.init()
 window = pygame.display.set_mode((500, 400))
 pygame.display.set_caption('Hello World!')
 
-amarelo = (204, 255, 0)
-azul = (0, 0, 153)
-
 # ----- Inicia estruturas de dados
 game = True
+
+# ----- Inicia assets
+font = pygame.font.SysFont(None, 48)
+text1 = font.render('HELLO', True, (0, 0, 255))
+text2 = font.render('WORLD', True, (0, 0, 255))
 
 # ===== Loop principal =====
 while game:
@@ -23,16 +25,13 @@ while game:
             game = False
 
     # ----- Gera saídas
-    window.fill((51, 156, 0))  # Preenche com a cor verde
-    vertices = [(250, 0), (500, 200), (250, 400), (0, 200)]
-    pygame.draw.polygon(window, amarelo, vertices)
-
-    # Adicionar circulo
-    centro = (250, 200)
-    pygame.draw.circle(window,azul, centro, 100)
+    window.fill((255, 255, 255))  # Preenche com a cor branca
+    window.blit(text1, (10, 10))
+    window.blit(text2, (10, 50))
 
     # ----- Atualiza estado do jogo
     pygame.display.update()  # Mostra o novo frame para o jogador
 
 # ===== Finalização =====
 pygame.quit()  # Função do PyGame que finaliza os recursos utilizados
+
