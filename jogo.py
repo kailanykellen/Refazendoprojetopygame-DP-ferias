fc4c07edbe275572648b8355f8c83ddf6af49fbc
Kailany <kailanykmm@al.insper.edu.br>
2021-08-12 07:30:26 -0300

    criação do primeiro meteoro

--- jogo.py
+++ jogo.py
@@ -1,21 +1,31 @@
 # ===== Inicialização =====
 # ----- Importa e inicia pacotes
 import pygame
+import random
 
 pygame.init()
 
 # ----- Gera tela principal
-WIDTH = 500
-HEIGHT = 400
+WIDTH = 480
+HEIGHT = 600
 window = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption('Hello World!')
+pygame.display.set_caption('Navinha') #trocamos o titulo
+
+# ----- Inicia assets
+METEOR_WIDTH = 50 #largura meteoro
+METEOR_HEIGHT = 38 #altura meteoro
+font = pygame.font.SysFont(None, 48)
+background = pygame.image.load('assets/img/starfield.png').convert() #imagem da tela que em breve será substituida
+meteor_img = pygame.image.load('assets/img/meteorBrown_med1.png').convert_alpha() #transparencia na imagem de meteoro
+meteor_img_small = pygame.transform.scale(meteor_img, (METEOR_WIDTH, METEOR_HEIGHT)) #diminuindo o tamanho do meteoro
 
 # ----- Inicia estruturas de dados
 game = True
-
-# ----- Inicia assets
-image = pygame.image.load('assets/img/logo-madfox.png').convert()
-image = pygame.transform.scale(image, (125, 166))
+meteor_x = 200
+# y negativo significa que está acima do topo da janela. O meteoro começa fora da janela
+meteor_y = -METEOR_HEIGHT #começar a cima da tela
+meteor_speedx = 3 #velocidade x
+meteor_speedy = 4 #velocidade y
 
 # ===== Loop principal =====
 while game:
@@ -25,11 +35,19 @@ while game:
         if event.type == pygame.QUIT:
             game = False
 
+    # ----- Atualiza estado do jogo
+    # Atualizando a posição do meteoro
+    meteor_x += meteor_speedx
+    meteor_y += meteor_speedy
+    # Se o meteoro passar do final da tela, volta para cima
+    if meteor_y > HEIGHT or meteor_x + METEOR_WIDTH < 0 or meteor_x > WIDTH:
+        meteor_x = 200
+        meteor_y = -METEOR_HEIGHT
+
     # ----- Gera saídas
     window.fill((0, 0, 0))  # Preenche com a cor branca
-    window.blit(image, (10, 10))
-
-    # ----- Atualiza estado do jogo
+    window.blit(background, (0, 0))
+    window.blit(meteor_img_small, (meteor_x, meteor_y))
     pygame.display.update()  # Mostra o novo frame para o jogador
 
 # ===== Finalização =====
